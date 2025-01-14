cbed4f747e855d5dd8300ad5b1a23513d8990cf3
Tijn van Elburg <6046216@mborijnland.nl>
2025-01-14 10:25:18 +0100

    database global gemaakt

--- functions.php
+++ functions.php
@@ -2,13 +2,12 @@
 
 function databaseConnect()
 {
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
+    $servername = "82.165.51.200";
+    $username = "u42_ZFmn4UQD3o";
+    $password = "z4v=y=rmN+wBtMSk+4ZiZBbF";
 
     try {
         $conn = new PDO("mysql:host=$servername;dbname=hersenhap", $username, $password);
-        // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo "Connected successfully";
     } catch (PDOException $e) {
